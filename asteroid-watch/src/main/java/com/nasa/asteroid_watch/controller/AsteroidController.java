306e8c3a4100890deabff56c5f5f251fb3ff22ee
emir <emir.d10@outlook.com>
2026-02-09 01:06:17 +0300

    spacing changes

--- asteroid-watch/src/main/java/com/nasa/asteroid_watch/controller/AsteroidController.java
+++ asteroid-watch/src/main/java/com/nasa/asteroid_watch/controller/AsteroidController.java
@@ -1,22 +1,3 @@
 package com.nasa.asteroid_watch.controller;
 
-import com.nasa.asteroid_watch.model.Asteroid;
-import com.nasa.asteroid_watch.repository.AsteroidRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-public class AsteroidController {
-
-    private final AsteroidRepository asteroidRepository;
-
-    @GetMapping("/asteroids")
-    public List<Asteroid> getAllAsteroids() {
-
-        return asteroidRepository.findAll();
-    }
-}
+import com.nasa.asteroid_watch.model
