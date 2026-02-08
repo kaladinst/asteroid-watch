99fa90234ce5ecd1d44bb48a347fe7111f2b1024
emir <emir.d10@outlook.com>
2026-02-09 01:06:36 +0300

    no changes

--- asteroid-watch/src/main/java/com/nasa/asteroid_watch/controller/AsteroidController.java
+++ asteroid-watch/src/main/java/com/nasa/asteroid_watch/controller/AsteroidController.java
@@ -1,3 +1,22 @@
 package com.nasa.asteroid_watch.controller;
 
-import com.nasa.asteroid_watch.model
+import com.nasa.asteroid_watch.model.Asteroid;
+import com.nasa.asteroid_watch.repository.AsteroidRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class AsteroidController {
+
+    private final AsteroidRepository asteroidRepository;
+
+    @GetMapping("/asteroids")
+    public List<Asteroid> getAllAsteroids() {
+
+        return asteroidRepository.findAll();
+    }
+}
